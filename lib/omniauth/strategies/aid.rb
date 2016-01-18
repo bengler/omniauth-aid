26fe5f2e0fe6d934c01c7f103774d36e6c80d29f
Jonas Helgemo <jonas.helgemo@gmail.com>
2016-01-18 12:10:03 +0100

    Cleanup and add support for new HAL-response format
    Why:
    
    * Amedia has decided to go with HAL as the default response format for json-apis

--- lib/omniauth/strategies/aid.rb
+++ lib/omniauth/strategies/aid.rb
@@ -1,25 +1,42 @@
 require 'omniauth/strategies/oauth2'
+require 'json'
 
 module OmniAuth
   module Strategies
-    class Aid  < OmniAuth::Strategies::OAuth2
+    # Class used to parse and return omniauth responses from aID
+    class Aid < OmniAuth::Strategies::OAuth2
       option :name, :aid
       option :authorize_options, [:scope]
 
-      option :client_options, {
-               site: "https://www.aid.no",
-               authorize_url: "/api/portunus/v1/oauth/authorize",
-               token_url: "/api/portunus/v1/oauth/token"
-             }
+      option :client_options,
+             site: 'https://www.aid.no',
+             authorize_url: '/api/portunus/v1/oauth/authorize',
+             token_url: '/api/portunus/v1/oauth/token'
 
-      uid { raw_info.fetch("data"){ {} }["id"] }
+      uid { raw_info['uuid'] || raw_info.key['id'] }
 
       info do
-        raw_info.fetch("data"){ {} }.fetch("attributes"){ {} }.to_h
+        prune!('id' => raw_info['id'],
+               'uuid' => raw_info['uuid'],
+               'nickname' => raw_info['name'],
+               'email' => raw_info['email'],
+               'name' => raw_info['name'],
+               'image' => raw_info
+                         .fetch('_links') { {} }
+                         .fetch('avatar') { {} }['href']
+              )
+      end
+
+      def prune!(hash)
+        hash.delete_if do |_, value|
+          prune!(value) if value.is_a?(Hash)
+          value.nil? || (value.respond_to?(:empty?) && value.empty?)
+        end
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/mercury/v2/users/me').parsed || {}
+        response_body = access_token.get('/api/mercury/v2/users/me').body
+        @raw_info ||= JSON.parse(response_body) || {}
       end
     end
   end
